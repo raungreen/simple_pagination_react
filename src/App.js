26d79adefac7b94dc3499b4ace8efbc7362b763c
Raun G <rgreen@creativeanvil.com>
2019-06-25 11:04:49 -0500

    added handleRequest

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(5);
+  const [postsPerPage, setPostsPerPage] = useState(10);
 
   const url = `https://jsonplaceholder.typicode.com/posts`;
 
@@ -31,11 +31,32 @@ const App = () => {
   // Change page
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
+  // Request per page
+  const handleRequest = e => {
+    e.preventDefault();
+    setPostsPerPage(e.target.value);
+  };
+
   // console.log(posts.length);
 
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">Home</h1>
+      <div className="form-group">
+        <div className="float-right" />
+        <label htmlFor="exampleFormControlSelect1">Posts per page</label>
+        <select
+          className="form-control col-1 float-right"
+          id="exampleFormControlSelect1"
+          value={postsPerPage}
+          onChange={handleRequest}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+        </select>
+      </div>
       <Posts posts={currentPosts} loading={loading} />
       <Pagination
         postsPerPage={postsPerPage}
